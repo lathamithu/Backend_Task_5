44787b771bff19903b6c33913f62aaeb691b9397
Meghana <lathamithu2000@gmail.com>
2020-07-12 11:30:01 +0530

    Store to database

--- insert.php
+++ insert.php
@@ -1,48 +1,29 @@
 <?php
-require 'class.php';
 
-class Connection{
+require 'connection.php';
 
-	private $dsn, $pdo, $user, $pass;
+$s = new Marks($_POST['name'],$_POST['date'],$_POST['gender'],$_POST['department'],$_POST['branch'],$_POST['int'],$_POST['sem']);
 
-	function __construct()
-	{
-
-		$this->dsn = "mysql:host=localhost;dbname=employee";
-		$this->user = "root";
-		$this->pass = "";
-
-	}
-
-	public function connect()
-	{
-		try
-		{
-			$this->pdo = new PDO($this->dsn,$this->user,$this->pass);
-		}
-		catch(PDOException $e)
-		{
-			echo "Failed" . $e->getMessage();
-		}
-	}
-
-	public function insert($Name,$Date_,$Gender,$Department,$Branch,$Internals,$Semester,$Total)
-	{
-		$sql = "INSERT INTO `student`(`Name`,`DOB`, `Gender`, `Department`, `Branch`, `Internals`, `Semester`, `Total`) VALUES (:name,:date_,:gender,:dept,:branch,:internals,:semester,:total)";
-		$stmt = $this->pdo->prepare($sql);
-
-		$pdoExec = $stmt->execute(array(":name"=>$Name,":date_"=>$Date_,":gender"=>$Gender,":dept"=>$Department,":branch"=>$Branch,":internals"=>$Internals,":semester"=>$Semester,":total"=>$Total));
-	}
-}	
-
+$s->validate();
 
 $con = new Connection();
 
 $con->connect();
 
-$s = new Student($_POST['name'],$_POST['date'],$_POST['gender'],$_POST['department'],$_POST['branch']);
-$m = new Marks($_POST['int'],$_POST['sem']);
 
-$con->insert($s->get_name(),$s->get_dob(),$s->get_gender(),$s->get_dept(),$s->get_branch(),$m->get_int(),$m->get_sem(), $m->calculate());
+if($s->areInputFieldsValid())
+{
+	$s->calculate();
+
+	$con->insert($s);
+
+}
+else
+{
+	session_start();
+	$con->storeSession($s);	
+	header("Location:stumain.php");
+}
+
 
 ?>
